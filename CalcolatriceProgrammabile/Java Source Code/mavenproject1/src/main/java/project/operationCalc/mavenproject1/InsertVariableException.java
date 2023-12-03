46af38649a4787300286da74a391567329ef98b9
LuciaNancyLavista <l.lavista1@studenti.unisa.it>
2023-12-03 09:11:11 +0100

    Insert Exception
    inserimento FullException che raggruppa EmptyStackException, ArithmeticException, InsertVariableException, WrongNumberException

---
+++ CalcolatriceProgrammabile/Java Source Code/mavenproject1/src/main/java/project/operationCalc/mavenproject1/InsertVariableException.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package com.mycompany.calcolatriceprogrammabile;
+
+/**
+ *
+ * @author nancy
+ */
+public class InsertVariableException extends Exception{
+
+    /**
+     * Creates a new instance of <code>InsertVariableException</code> without
+     * detail message.
+     */
+    public InsertVariableException() {
+    }
+
+    /**
+     * Constructs an instance of <code>InsertVariableException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public InsertVariableException(String msg) {
+        super(msg);
+    }
+}
