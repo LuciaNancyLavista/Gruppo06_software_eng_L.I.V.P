2c73dc51ed6fdb8a98df2d862566f32d481e8e12
LuciaNancyLavista <150945828+LuciaNancyLavista@users.noreply.github.com>
2023-12-11 23:43:24 +0100

    Update InserisciNumero.java
    Update comments

--- CalcolatriceProgrammabile/Java Source Code/Calcolatriceprogrammabile/src/main/java/com/mycompany/cal/InserisciNumero.java
+++ CalcolatriceProgrammabile/Java Source Code/Calcolatriceprogrammabile/src/main/java/com/mycompany/cal/InserisciNumero.java
@@ -10,14 +10,17 @@ package com.mycompany.cal;
  */
 public class InserisciNumero {
 
+    // metodo inserisci per analizzare il numero che si è scritto nel text field della calcolatrice
     public void inserisci(String var, StackNum s){
         double newReal;
         double newImg;
 
+        // suddivisione in reale e immaginario, i quali si distinguono da uno spazio insirito tra i due
         String[] parameters = var.split(" ");
         newReal = Double.parseDouble(parameters[0]);
         newImg  = Double.parseDouble(parameters[1]);
 
+        // inserimento del numero complesso nello stack
         s.push(new Complex(newReal, newImg));
     }
 
