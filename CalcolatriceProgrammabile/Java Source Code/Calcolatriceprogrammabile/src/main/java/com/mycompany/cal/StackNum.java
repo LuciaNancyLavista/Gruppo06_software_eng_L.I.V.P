5a017a9f09ce9cf4a438491a67c835962b34e93d
LuciaNancyLavista <150945828+LuciaNancyLavista@users.noreply.github.com>
2023-12-11 23:52:04 +0100

    Update StackNum.java
    Update comments

--- CalcolatriceProgrammabile/Java Source Code/Calcolatriceprogrammabile/src/main/java/com/mycompany/cal/StackNum.java
+++ CalcolatriceProgrammabile/Java Source Code/Calcolatriceprogrammabile/src/main/java/com/mycompany/cal/StackNum.java
@@ -11,51 +11,63 @@ import java.util.LinkedList;
  * @author luigi
  */
 public class StackNum {
+    //dichiarazione di una LinkedList per lo stack di tipo Complex, così da poterci ritornare un complesso
     private LinkedList<Complex> stack;
     
-    
+    // definizione costruttore con la lista da dichiarare
     public StackNum(){
         stack = new LinkedList<>();
     }
-    
+
+    // metodo push per inserire il numero complesso nello stack
     public void push(Complex c){
         stack.addFirst(c);
     }
-    
+
+    // metodo top per acquisire il numero complesso in testa dello stack
     public Complex top(){
         return stack.getFirst();
     }
-    
+
+    // metodo duplicate per inserire il numero complesso (top) duplicato nello stack
     public void dup(){
         Complex temp = stack.getFirst();
         stack.addFirst(temp);    
     }
-    
+
+    // metodo drop per rimuovere il numero complesso in testa dallo stack
     public Complex drop(){
         return stack.removeFirst();
     }
-    
+
+    // metodo clear per pulire lo stack
     public void clear(){
         stack.clear();
         
     }
+
+    // metodo over per duplicare il penultimo elemento dello stack e inserimento in testa
     public void over(){
         stack.addFirst(stack.get(1));
-       
     }
-   
+
+    // metodo swap per scambiare gli ultimi due numeri nello stack
     public void swap(){
+        // prima si eliminano dallo stack
         Complex e1 = stack.removeFirst();
         Complex e2 = stack.removeFirst();
+
+        // posizionamento nello stack
         stack.addFirst(e1);
         stack.addFirst(e2);   
     }
-    
+
+    // metodo isEmpty per verifica se vuoto lo stack
     public boolean isEmpty(){
         return stack.isEmpty();
     }
-    
-    
+
+    // metodo toString per la stampa dello stack
     @Override
     public String toString(){
         return stack.toString();
