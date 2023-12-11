cfa958f19718eee6ed7e63069b073f0eefefe42a
LuciaNancyLavista <150945828+LuciaNancyLavista@users.noreply.github.com>
2023-12-11 23:37:02 +0100

    Update InserisciOperazioni.java
    Update comments and exception

--- CalcolatriceProgrammabile/Java Source Code/Calcolatriceprogrammabile/src/main/java/com/mycompany/cal/InserisciOperazioni.java
+++ CalcolatriceProgrammabile/Java Source Code/Calcolatriceprogrammabile/src/main/java/com/mycompany/cal/InserisciOperazioni.java
@@ -10,46 +10,86 @@ package com.mycompany.cal;
  */
 public class InserisciOperazioni {
 
-    // class methods
+    // Implementazione metodo Addition: somma di due numeri complessi
     public void add(StackNum s){
+
+	// controllo se sono stati inseriti almeno 2 numeri richiamando l'eccezione sullo stack
+	if (s.stack.size() < 2) 
+            throw new EmptyStackException("Inserire almeno due numeri nello stack. ");
+
+	// prelevamento dallo stack
         Complex c1 = s.drop();
         Complex c2 = s.drop();
+	    
+	// divido in parte reale ed immaginaria così da poter effettuare la somma 
         double newReal = c1.getReal() + c2.getReal();
         double newImg = c1.getImaginary() + c2.getImaginary();
-	    s.push(new Complex(newReal, newImg));
+	    
+	//inserimento nello stack del risultato
+	s.push(new Complex(newReal, newImg));
     }
 
+    // Implementazione metodo Substraction: sottrazione tra due numeri complessi
     public void sub(StackNum s){
+
+	// controllo se sono stati inseriti almeno 2 numeri richiamando l'eccezione sullo stack
+	if (s.stack.size() < 2) 
+            throw new EmptyStackException("Inserire almeno due numeri nello stack. ");
+
+	// prelevamento dallo stack
         Complex c2 = s.drop();
         Complex c1 = s.drop();
+
+	// divido in parte reale ed immaginaria così da poter effettuare la sottrazione 
         double newReal = c1.getReal() - c2.getReal();
 	double newImg = c1.getImaginary() - c2.getImaginary();
+
+	//inserimento nello stack del risultato
         s.push(new Complex(newReal,newImg));
     }
 
+	// Implementazione metodo Multiplication: moltiplicazione di due numeri complessi
     public void mult(StackNum s){
+
+	// controllo se sono stati inseriti almeno 2 numeri richiamando l'eccezione sullo stack
+	if (s.stack.size() < 2) 
+            throw new EmptyStackException("Inserire almeno due numeri nello stack. ");
+
+	// prelevamento dallo stack
         Complex c1 = s.drop();
         Complex c2 = s.drop();
-	    double newReal = c1.getReal()*c2.getReal() - c1.getImaginary()*c2.getImaginary();
-	    double newImg = c1.getReal()*c2.getImaginary() + c1.getImaginary()*c2.getReal();
-	    s.push(new Complex(newReal, newImg));	
+
+	// divido in parte reale ed immaginaria così da poter effettuare la moltiplicazione 
+	double newReal = c1.getReal()*c2.getReal() - c1.getImaginary()*c2.getImaginary();
+    	double newImg = c1.getReal()*c2.getImaginary() + c1.getImaginary()*c2.getReal();
+
+	//inserimento nello stack del risultato
+	s.push(new Complex(newReal, newImg));	
     }
 
-    
+    // Implementazione metodo Division: divisione tra due numeri complessi
     public void div(StackNum s){
+
+	// controllo se sono stati inseriti almeno 2 numeri richiamando l'eccezione sullo stack
+	if (s.stack.size() < 2) 
+            throw new EmptyStackException("Inserire almeno due numeri nello stack. ");
+
+	// prelevamento dallo stack
         Complex c2 = s.drop();
         Complex c1 = s.drop();
-	
-	
+
+	// se la parte immaginare del divisore è uguale a 0 allora lancio l'eccezione aritmetica
+	if (c2.getImaginary() == 0) 
+            throw new ArithmeticException("Errore: Divisione per zero. ");
+	    
+	// divido in parte reale ed immaginaria così da poter effettuare la divisione 
 	double zreal = 0;
 	double zimag = 0;
-	
 	if(c2.getReal() != 0)
 		zreal = Math.pow(c2.getReal(),2);
 	if(c2.getImaginary() != 0)
 		zimag = Math.pow(c2.getImaginary(),2);
         
-
 	double ac = c1.getReal()*c2.getReal();
 	double bd = c1.getImaginary()*c2.getImaginary();
 	double bc = c1.getReal()*c2.getReal();
@@ -58,11 +98,21 @@ public class InserisciOperazioni {
 	double newReal = (ac+bd)/(zreal+zimag);
 	double newImag = (bc-ad)/(zreal+zimag);
 
+	// inserisco il risultato nello stack
 	s.push(new Complex(newReal,newImag));
     }	
 
+	// Implementazione metodo Square Root: radice quadrata di un numero complesso
     public void sqrt(StackNum s){
+
+	// controllo se lo stack è vuoto (ci serve solo un numero per sqrt) lanciando l'eccezione che riguarda lo stack
+	if (s.stack.isEmpty()) 
+            throw new EmptyStackException("Stack vuoto: serve almeno un numero. ");
+
+	// prelevamento dallo stack
         Complex c = s.drop();
+
+	// assegnazione di reale ed immaginario per poter effettuare la radice quadrata
 	double newReal;
         double newImg;
         if(c.getImaginary() != 0){
@@ -79,13 +129,23 @@ public class InserisciOperazioni {
             newReal = Math.sqrt(c.getReal());
             newImg = 0;
         }
+	    // scrittura nello stack, sono due risultati visto che la radice quadrata ritorna due soluzioni
 	    s.push(new Complex(newReal, newImg));
             s.push(new Complex(newReal, newImg));
             
     }
 
+	// Implementazione metodo Inversion Sign: inversione del segno di un numero complesso
     public void invSign(StackNum s){
+
+	// controllo se lo stack è vuoto (visto che ci serve solo un numero per questa operazione) lanciando un'eccezione sullo stack
+	if (s.stack.isEmpty()) 
+	    throw new EmptyStackException("Stack vuoto: serve almeno un numero. ");
+
+	// prelevamento dallo stack
         Complex c = s.drop();
+
+	// scrittura del risultato nello stack
         s.push(new Complex(-1.0*c.getReal(),-1.0*c.getImaginary()));
     }
 
