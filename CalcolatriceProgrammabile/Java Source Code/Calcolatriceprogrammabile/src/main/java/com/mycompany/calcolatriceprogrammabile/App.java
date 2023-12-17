c6a6f7ac7629deb6e996641165ba965da40e6aa4
LuciaNancyLavista <150945828+LuciaNancyLavista@users.noreply.github.com>
2023-12-17 11:40:59 +0100

    Update App.java

--- CalcolatriceProgrammabile/Java Source Code/Calcolatriceprogrammabile/src/main/java/com/mycompany/calcolatriceprogrammabile/App.java
+++ CalcolatriceProgrammabile/Java Source Code/Calcolatriceprogrammabile/src/main/java/com/mycompany/calcolatriceprogrammabile/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 729, 562);
+        scene = new Scene(loadFXML("primary"), 740, 570);
         stage.setTitle("Programmable Calculator LIVP");
         stage.getIcons().add(new Image("file:calculator.png"));
         stage.setResizable(false);
